09b38bad7d882da1b449bd6e9c7804fa80668044
Denis Wachira <deniswachira77@gmail.com>
2024-07-16 16:01:43 +0300

    proxxy setup

--- src/proxxy/proxxy.tsx
+++ src/proxxy/proxxy.tsx
@@ -1,2 +1,5 @@
 export const apiDomain = 'https://ridewave.azurewebsites.net/';
 export const apiDomain1 = 'http://localhost:8000/';
+
+// export const apiCheckout = 'http://localhost:8000';
+export const apiCheckout = 'https://ridewave.azurewebsites.net';
